b5fd614144b01b056ddcfc3a4d4776595fab2969
Carlos Roque <email@email.com>
2023-03-28 11:04:17 -0300

    aula de introdução ao c# - Tipos de dados, variáveis, constantese operadores

---
+++ manha/Backend-I/Program.cs
@@ -0,0 +1,99 @@
+﻿// string name = "Carlos";
+// Console.WriteLine(name);
+
+// int idade = 36;
+// idade = 50;
+
+// const int idade = 36;
+// idade = 45;
+
+// Console.WriteLine("A idade do " + name + " é " + idade + " anos");
+
+// float preco = 10.95f;
+
+// bool careca = false;
+// Console.WriteLine(careca);
+
+// Operadores
+
+// atribuição =
+// int x = 15;
+
+// operadores aritméticos
+
+// soma
+// Console.WriteLine(5 + 4);
+// Console.WriteLine(5 + "4");
+// Console.WriteLine(5 + x);
+
+// subtração
+// Console.WriteLine(15 - 3);
+
+// multiplicação
+// Console.WriteLine(5 * 5);
+
+// divisão
+// Console.WriteLine(10 / 10);
+
+// modular
+// Console.WriteLine(5 % 2);
+
+
+// //operadores de comparação
+
+// //igual a
+// Console.WriteLine(5 == 7);
+
+// // maior ou igual a
+// Console.WriteLine(5 >= 10);
+
+// //menor ou igual a
+// Console.WriteLine(4 <= 10);
+
+// //maior que
+// Console.WriteLine(150 > 95);
+
+// //menor que
+// Console.WriteLine(54 < 54);
+
+// //diferente de
+// Console.WriteLine( 5 != 6);
+
+//operadores lógicos
+
+// && : e
+// || : ou 
+// ! : não
+
+// Console.WriteLine(5 == 5 && 8 == 8);// TRUE && TRUE = TRUE
+// Console.WriteLine(5 == 5 && 8 == 5);// TRUE && FALSE = FALSE
+// Console.WriteLine(5 == 6 && 8 == 8);// FALSE && TRUE = FALSE
+// Console.WriteLine(5 == 6 && 8 == 5);// FALSE && FALSE = FALSE
+
+// Console.WriteLine(2 == 2 || 3 == 3);// TRUE || TRUE = TRUE
+// Console.WriteLine(2 == 2 || 3 == 4);// TRUE || FALSE = TRUE
+// Console.WriteLine(2 == 4 || 3 == 3);// FALSE || TRUE = TRUE
+// Console.WriteLine(2 == 4 || 3 == 5);// FALSE || FALSE = FALSE
+
+// Console.WriteLine((2 == 4 || 4 == 4) && (7 == 6));
+
+// Console.WriteLine(!(2 == 2 || 3 == 3));
+
+//Crie um programa para calcular o IMC de uma pessoa
+
+//Algoritmo
+//entrada: nome, idade, peso, altura
+//processamento: peso dividido por altura ao quadrado
+
+// //entrada
+// string nome = "Eduardo";
+// int idade = 40;
+// float peso = 94;
+// float altura = 1.67f;
+
+// //processamento
+// float imc = peso / (altura * altura);
+
+// //saída
+// Console.WriteLine($"O IMC do " + nome + " é de : " + Math.Round(imc,2));
+
