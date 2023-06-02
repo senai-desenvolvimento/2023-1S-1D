03b47e4bb2e6550ac69d9c6b358acf63e093c12d
Carlos Roque <roqueaugustocarlos@gmail.com>
2023-06-02 17:21:13 -0300

    atualizado repositório

---
+++ Tarde/Backend-II/projeto-gamer-tarde/Controllers/JogadorController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using projeto_gamer_tarde.Infra;
+
+namespace projeto_gamer_tarde.Controllers
+{
+    [Route("[controller]")]
+    public class JogadorController : Controller
+    {
+        private readonly ILogger<JogadorController> _logger;
+
+        public JogadorController(ILogger<JogadorController> logger)
+        {
+            _logger = logger;
+        }
+
+        Context c = new Context();
+
+        [Route("Listar")]
+        public IActionResult Index()
+        {
+            ViewBag.Jogador = c.Jogador.ToList();
+            ViewBag.Equipe = c.Equipe.ToList();
+
+            return View();
+        }
+
+        [Route("Cadastrar")]
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            // Implementar o método de cadastro
+
+            return LocalRedirect("");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
